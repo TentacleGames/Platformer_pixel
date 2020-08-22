9a126dc162fd4284d91de6c7e3847a1f15799930
shtabel <shtabel@live.com>
2020-08-23 00:04:24 +0300

    pickups added
    with some cheap effects

--- Assets/Resources/Scripts/HealthSystem.cs
+++ Assets/Resources/Scripts/HealthSystem.cs
@@ -6,10 +6,22 @@ public class HealthSystem : MonoBehaviour
     public int health;
     public int maxHealth;
 
+    private int maxHitCooldown = 100;
+    private int hitCooldown = 0;
+    private bool isHit = false;
+
+    private int maxHealCooldown = 50;
+    private int healCooldown = 0;
+    private bool isHealed = false;
+
+
     public void TakeHit(int damage)
     {
+        if (isHit)
+            return;
         health -= damage;
-
+        hitCooldown = maxHitCooldown;
+        isHit = true;
         if (health <= 0)
             Destroy(gameObject);
     }
@@ -17,8 +29,43 @@ public class HealthSystem : MonoBehaviour
     public void AddHealth(int health)
     {
         this.health += health;
-        
+        isHealed = true;
+        healCooldown = maxHealCooldown;
         if (this.health > maxHealth)
             this.health = maxHealth;
     }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (health < maxHealth && col.gameObject.CompareTag("Health")){
+            AddHealth(1);
+            Destroy(col.gameObject);
+        }
+    }
+
+    public void Update()
+    {
+        if (isHit){
+            SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+            sr.color = Color.red;
+            hitCooldown--;
+            if (hitCooldown <= 0){
+                sr.color = Color.white;
+                isHit = false;
+            }
+        }else
+        if (isHealed){
+            SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+            sr.color = Color.cyan;
+            healCooldown--;
+            if (healCooldown <= 0){
+                sr.color = Color.white;
+                isHealed = false;
+            }
+        }
+            
+        
+    }
+
+
 }
---
+++ Assets/Resources/Scripts/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int coinsCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Coin")){
+            Destroy(col.gameObject);
+            coinsCount++;
+            Debug.Log("Coins: " + coinsCount);
+        }
+    }
+
+}
