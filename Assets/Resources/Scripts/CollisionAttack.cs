d637848acf3df6eabeed9cb7602254e1a84ff133
shtabel <shtabel@live.com>
2020-08-22 21:16:08 +0300

    Initial commit

---
+++ Assets/Resources/Scripts/CollisionAttack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAttack : MonoBehaviour
+{
+    public int damage = 1;
+    public string collisionTag;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag(collisionTag)){
+            int k = UnityEngine.Random.Range(10,100);
+            HealthSystem hpSystem = col.gameObject.GetComponent<HealthSystem>();
+            if (hpSystem != null)
+                hpSystem.TakeHit(damage);
+
+        }
+    
+    }
+
+
+}
---
+++ Assets/Resources/Scripts/EnemyPatrol.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public GameObject leftBorder, rightBorder;
+    public bool isRightDirection;
+    private Rigidbody2D rigidbody;
+    private GroundDetector groundDetecor;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        if (leftBorder is null || rightBorder is null){
+            string msg = "There is a problem with borders in unit " + this.name;
+            Debug.Log(msg);
+        }
+        groundDetecor = GetComponent<GroundDetector>();
+        if (groundDetecor is null)
+            Debug.Log("Can't find groundDetecor for "+name);
+            
+        
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        if (groundDetecor.isGrounded){
+            float k = speed * (isRightDirection ? 1 : -1);
+            rigidbody.velocity = new Vector2(k, 0);
+
+            if (isRightDirection && transform.position.x >= rightBorder.transform.position.x ||
+                !isRightDirection && transform.position.x <= leftBorder.transform.position.x
+                )
+                isRightDirection = !isRightDirection;
+        }
+    }
+}
